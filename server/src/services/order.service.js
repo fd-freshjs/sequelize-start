51e61cf770b8900a8e4bf87102afb197c6ddb9ab
Yevhenii <freshjs.fte.@gmail.com>
2022-06-30 17:17:26 +0300

    Add create order service

--- server/src/services/order.service.js
+++ server/src/services/order.service.js
@@ -1,29 +1,60 @@
 // createOrder
 
 const createHttpError = require("http-errors");
-const { Order, Product, ProductsToOrders } = require("../db/models");
+const { Order, Product, ProductsToOrders, Sequelize, sequelize } = require("../db/models");
+
+/* 
+
+
+  UPDATE products
+  SET amount = CASE
+    WHEN id = 1 THEN 3 WHEN id = 2 THEN amount - 6
+    ELSE 0
+  END
+
+  WHERE id IN (1, 2, 3);
+
+*/
 
 class OrderService {
   createOrder = async (data = {}) => {
     try {
       const { cart = [], ...restData } = data;
 
-      const prodIds = cart.map(c => c.product_id); // [1, 2, 3, 4]
-      const products = await Product.findAll({ where: { id: prodIds } });
+      const transaction = await sequelize.transaction();
 
+      const prodIds = cart.map((c) => c.product_id); // [1, 2, 3, 4]
+      const products = await Product.findAll({ where: { id: prodIds } });
       if (products.length !== prodIds.length) {
         throw createHttpError(404, "Products not found");
       }
 
-      const newOrder = await Order.create(restData);
-      
-      const insertRows = cart.map(prod => {
+      const newOrder = await Order.create({ ...restData, status: 'prepared' });
+      const insertRows = cart.map((prod) => {
         return { ...prod, order_id: newOrder.id };
       });
       await ProductsToOrders.bulkCreate(insertRows);
 
+      // update product amounts
+      await Product.update(
+        {
+          amount: Sequelize.literal(
+            `CASE
+            ${cart
+              .map((c) => `WHEN id = ${c.product_id} THEN amount - ${c.amount}`)
+              .join(" ")}
+              ELSE 0
+              END`
+          ),
+        },
+        { where: { id: prodIds } },
+      );
+
+      await transaction.commit();
       return newOrder;
     } catch (error) {
+      await transaction.rollback();
+
       if (error.message.includes("orders_phone_ck")) {
         throw createHttpError(
           400,
@@ -71,6 +102,13 @@ class OrderService {
     });
     if (count === 0) {
       throw createHttpError(404, "Order not found");
+    }
+    if (data.cart) {
+      // check products existance
+      // add products to order
+    }
+    if (data.cartRemove) {
+      // remove products from order
     }
 
     const updatedOrder = await this.findOrderById(id);
